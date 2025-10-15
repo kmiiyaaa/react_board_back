01b75a5d5db625f9347b766f2969b0620ff341ea
kmiiyaaa <ckm1533@naver.com>
2025-10-15 11:06:57 +0900

    회원가입 validation추가

--- src/main/java/com/kmii/home/controller/AuthController.java
+++ src/main/java/com/kmii/home/controller/AuthController.java
@@ -1,19 +1,25 @@
 package com.kmii.home.controller;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kmii.home.dto.SiteUserDto;
 import com.kmii.home.entity.SiteUser;
 import com.kmii.home.repository.UserRepository;
+
+import jakarta.validation.Valid;
 
 
 @RestController
@@ -26,17 +32,63 @@ public class AuthController {
 	@Autowired
 	private PasswordEncoder passwordEncoder;
 	
-	@PostMapping("/signup") // sql -> insert -> post
-	public ResponseEntity<?> signup(@RequestBody SiteUser req) { // <?> 해놓으면 문자든 객체든 다 들어올 수 있다.
+//	@PostMapping("/signup") // sql -> insert -> post
+//	public ResponseEntity<?> signup(@RequestBody SiteUser req) { // <?> 해놓으면 문자든 객체든 다 들어올 수 있다.
+//		//사용자 이름(username)이 DB에 이미 존재하는지 확인
+//		if(userRepository.findByUsername(req.getUsername()).isPresent()) {
+//			//참이면->이미지 해당 username(아이디)가 존재하므로 가입 불가
+//			//거짓이면->가입 가능
+//			return ResponseEntity.badRequest().body("이미 존재하는 사용자명 입니다."); //가입실패 -> 에러 메시지
+//		}
+//		req.setPassword(passwordEncoder.encode(req.getPassword())); 
+//		//비밀번호 암호화해서 엔티티에 다시 넣기
+//		userRepository.save(req);
+//		
+//		return ResponseEntity.ok("회원가입 성공!"); //가입성공 -> 성공메시지
+//		//return ResponseEntity.ok(req); //가입성공 후 해당 엔티티 반환
+//	}
+//	
+	
+	
+	//validation 적용 회원가입 (빈칸 입력 방지, 최소 4글자 이상)
+	@PostMapping("/signup")
+	public ResponseEntity<?> signup(@Valid @RequestBody SiteUserDto siteUserDto, BindingResult bindingResult) { //siteUser라는 엔티티티가 직접 받는게아니라 dto가 대신 받는다. (위 원본과 비교)
+		
+		//spring validation 결과 처리
+		if(bindingResult.hasErrors()) { //참이면 유효성 체크 실패 -> err
+			//restful api라서 다른데로 돌려보내는거 안해줘도된다. 에러만 표시해주면 된다.
+			
+			Map<String, String> errors = new HashMap<>();  // username, password필드가 String, message가 String이라서 <String,String>
+			bindingResult.getFieldErrors().forEach(  //에러의 갯수는 여러개일 확률이 높아서 getfielderrors로 넣는다.
+					err -> {
+						errors.put(err.getField(), err.getDefaultMessage());
+						// 회원 가입시 -> username:abc, password:123 시 에러나면
+						// {
+						//"username":"아이디는 최소 4글자 이상입니다.", 
+						//"password":"비밀번호는 최소 4글자 이상입니다."
+						// }
+					}
+				);		
+			
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors); // 에러나면 위에 적은 {~~} 값이 반환된다
+			
+			}
+		
+		SiteUser siteUser = new SiteUser();  //Entity 객체 선언
+		//사용자가 입력한 username(dto에 존재)을 entity객체에 set
+		siteUser.setUsername(siteUserDto.getUsername());
+		//사용자가 입력한 password(dto에 존재)를 entity객체에 set
+		siteUser.setPassword(siteUserDto.getPassword());
+		
 		//사용자 이름(username)이 DB에 이미 존재하는지 확인
-		if(userRepository.findByUsername(req.getUsername()).isPresent()) {
+		if(userRepository.findByUsername(siteUser.getUsername()).isPresent()) {
 			//참이면->이미지 해당 username(아이디)가 존재하므로 가입 불가
 			//거짓이면->가입 가능
 			return ResponseEntity.badRequest().body("이미 존재하는 사용자명 입니다."); //가입실패 -> 에러 메시지
 		}
-		req.setPassword(passwordEncoder.encode(req.getPassword())); 
+		siteUser.setPassword(passwordEncoder.encode(siteUser.getPassword())); 
 		//비밀번호 암호화해서 엔티티에 다시 넣기
-		userRepository.save(req);
+		userRepository.save(siteUser);
 		
 		return ResponseEntity.ok("회원가입 성공!"); //가입성공 -> 성공메시지
 		//return ResponseEntity.ok(req); //가입성공 후 해당 엔티티 반환
--- src/main/java/com/kmii/home/controller/BoardController.java
+++ src/main/java/com/kmii/home/controller/BoardController.java
@@ -24,100 +24,103 @@ import com.kmii.home.repository.UserRepository;
 @RestController
 @RequestMapping("/api/board")
 public class BoardController {
-	
-	@Autowired
-	private BoardRepository boardRepository;
-	
-	@Autowired
-	private UserRepository userRepository;
-	
-	//전체 게시글 조회
-	@GetMapping
-	public List<Board> list() {
-		return boardRepository.findAll();
-	}
-	
-	//게시글 작성
-	@PostMapping
-	public ResponseEntity<?> write(@RequestBody Board req, Authentication auth) {
-		
-		//auth.getName() -> 로그인 유저이름
-		
-		SiteUser siteUser = userRepository.findByUsername(auth.getName())
-				.orElseThrow(() -> new UsernameNotFoundException("사용자 없움"));
-		//siteUser -> 현재 로그인한 유저의 레코드
-		
-		//예전 방식
-		Board board = new Board();
-		board.setTitle(req.getTitle());  //user가 입력한 글 제목
-		board.setContent(req.getContent()); //입력 글 내용
-		board.setAuthor(siteUser); //유저정보
-		
-		boardRepository.save(board);
-		
-		return ResponseEntity.ok(board);
-	}
-	
-	//특정 게시글 번호(id)로 조회(글 상세보기) , select 조회하는거니까 getmapping
-	@GetMapping("/{id}")
-	public ResponseEntity<?> getPost(@PathVariable("id") Long id) {
-//		Board board = boardRepository.findById(id)
-//				.orElseThrow(() -> new UsernameNotFoundException("해당글 없움"));
-		
-		Optional<Board> _board = boardRepository.findById(id);
-		if(_board.isPresent()) {
-			return ResponseEntity.ok(_board.get()); //해당 id글을 반환 
-		}else { //거짓이면 해당글 조회 실패			
-			return ResponseEntity.status(404).body("해당 게시글 존재하지 않음");
-		}
-	}
-	
-	//특정 id 글 삭제
-	@DeleteMapping("/{id}") 
-	public ResponseEntity<?> deletePost(@PathVariable("id") Long id, Authentication auth){
-		
-		Optional<Board> _board = boardRepository.findById(id);
-		
-		//삭제할 글의 존재 여부 확인
-		if(_board.isPresent()) {
-			return ResponseEntity.status(404).body("해당 게시글은 존재하지 않아 삭제 실패");	
-		}
-		
-		//로그인한 유저의 삭제 권한 확인
-		if(auth == null || !auth.getName().equals(_board.get().getAuthor().getUsername())) { //auth.getName:현재 로그인한 id
-			return ResponseEntity.status(403).body("해당 게시글에 대한 삭제 권한이 없습니다.");
-		}
-			boardRepository.delete(_board.get());
-			return ResponseEntity.ok("삭제 완료");	
-		}
-	
-	//게시글 수정(권한 설정 -> 로그인 후 본인 작성글만 수정 가능)
-	@PutMapping("/{id}")
-	public ResponseEntity<?> updatePost(
-			@PathVariable("id") Long id, 
-			@RequestBody Board updateBoard, 
-			Authentication auth) {
-		
-		Optional<Board> _board = boardRepository.findById(id);
-		
-		if(_board.isEmpty()) {
-			return ResponseEntity.status(404).body("해당 게시글이 존재하지 않습니다.");
-		}
-		
-		if(auth == null || !auth.getName().equals(_board.get().getAuthor().getUsername())) { //auth.getName:현재 로그인한 id
-			return ResponseEntity.status(403).body("해당 게시글에 대한 수정 권한이 없습니다.");
-		}
-		
-		Board oldPost = _board.get(); //기존 게시글
-		
-		oldPost.setTitle(updateBoard.getTitle());
-		oldPost.setContent(updateBoard.getContent());
-		
-		boardRepository.save(oldPost); // 수정한 내용 저장
-
-		return ResponseEntity.ok(_board);
-		
-	}
-	
+   
+   @Autowired
+   private BoardRepository boardRepository;
+   
+   @Autowired
+   private UserRepository userRepository;
+   
+   //전체 게시글 조회
+      @GetMapping
+      public List<Board> list() {
+         return boardRepository.findAll();
+      }
+      
+      //게시글 작성
+      @PostMapping
+      public ResponseEntity<?> write(@RequestBody Board req, Authentication auth) {
+         
+         //auth.getName() -> 로그인한 유저 이름
+         
+         SiteUser siteUser = userRepository.findByUsername(auth.getName())
+               .orElseThrow(()->new UsernameNotFoundException("사용자 없음"));
+         //siteUser->현재 로그인한 유저의 레코드
+         
+         Board board = new Board();
+         board.setTitle(req.getTitle()); //유저가 입력한 글 제목
+         board.setContent(req.getContent()); //유저가 입력한 글 내용
+         board.setAuthor(siteUser); //유저 정보
+         
+         boardRepository.save(board);
+         
+         return ResponseEntity.ok(board);
+      }
+      
+      //특정 게시글 번호(id)로 조회(글 상세보기)
+      @GetMapping("/{id}")
+      public ResponseEntity<?> getPost(@PathVariable("id") Long id) {
+//         Board board = boardRepository.findById(id)
+//               .orElseThrow(()->new EntityNotFoundException("해당 글 없음"));
+         Optional<Board> _board = boardRepository.findById(id);
+         if(_board.isPresent()) { //참이면 글 조회 성공
+            return ResponseEntity.ok(_board.get()); //해당 id글을 반환
+         } else { //거짓이면 해당 글 조회 실패
+            return ResponseEntity.status(404).body("해당 게시글은 존재하지 않습니다.");
+         }
+         
+      }
+      
+      //특정 id 글 삭제(삭제권한->로그인한 후 본인 글만 삭제 가능)
+      @DeleteMapping("/{id}")
+      public ResponseEntity<?> deletePost(@PathVariable("id") Long id, Authentication auth) {  
+    	  //Authentocation대신 Principal써도된다. Authentocation 선호 - 현재 인증받은 사용자 가져올 수 있다.
+    	  // 옛날에 getsession에서 빼서 쓴거대신 auth~ 써서 해주는것 
+         Optional<Board> _board = boardRepository.findById(id);
+         
+         //삭제할 글의 존재 여부 확인
+         if (_board.isEmpty()) { //참이면 삭제할 글이 존재하지 않음         
+            return ResponseEntity.status(404).body("해당 게시글은 존재하지 않아 삭제 실패하였습니다.");
+         }
+         
+         //로그인한 유저의 삭제 권한 확인
+         if (auth == null || !auth.getName().equals(_board.get().getAuthor().getUsername())) {
+            return ResponseEntity.status(403).body("해당 글에 대한 삭제 권한이 없습니다.");
+         }
+         
+         
+         boardRepository.delete(_board.get());
+         return ResponseEntity.ok("글 삭제 성공");
+         
+      } 
+      
+      //게시글 수정(권한 설정->로그인 후 본인 작성글만 수정 가능)
+      @PutMapping("/{id}")
+      public ResponseEntity<?> updatePost(  
+            @PathVariable("id") Long id,  //아이디값은 따로 받아야한다, board에는 title, content 바뀐거만 옴
+            @RequestBody Board updateBoard, 
+            Authentication auth) {
+         
+         Optional<Board> _board = boardRepository.findById(id);
+         
+         if (_board.isEmpty()) { //참이면 수정할 글이 존재하지 않음
+            return ResponseEntity.status(404).body("해당 게시글이 존재하지 않습니다.");
+         }
+         
+         if (auth == null || !auth.getName().equals(_board.get().getAuthor().getUsername())) {
+            return ResponseEntity.status(403).body("해당 글에 대한 수정 권한이 없습니다.");
+         }
+         
+         Board oldPost = _board.get(); //기존 게시글
+         
+         oldPost.setTitle(updateBoard.getTitle()); //제목 수정
+         oldPost.setContent(updateBoard.getContent()); //내용 수정
+         
+         boardRepository.save(oldPost); //수정한 내용 저장
+         
+         return ResponseEntity.ok(oldPost); //수정된 내용이 저장된 글 객체 반환
+      }
+      
+   
 
 }
