ef3f53f09f43312eb5d4ad4d8400dc64a333b6e9
kmiiyaaa <ckm1533@naver.com>
2025-10-16 11:07:08 +0900

    댓글 리스트 조회 매핑 추가

--- src/main/java/com/kmii/home/controller/CommentController.java
+++ src/main/java/com/kmii/home/controller/CommentController.java
@@ -1,10 +1,12 @@
 package com.kmii.home.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -34,7 +36,7 @@ public class CommentController {
 	@Autowired
 	private BoardRepository boardRepository;
 	
-	//댓글 작성 -> 
+	//댓글 작성
 	@PostMapping("/{boardId}")
 	public ResponseEntity<?> writeComment(
 			@PathVariable("boardId") Long boardId,
@@ -59,6 +61,23 @@ public class CommentController {
 		
 		
 		return ResponseEntity.ok(comment); //db에 등록된 댓글 객체를 200 응답과 함께 반환
+	}
+	
+	
+	//댓글 조회 -> 원 게시글의 id가 있어야지만 볼 수 있다. -> 게시글 id로 댓글 조회 -> boardId가 전송받고 -> comments 조회
+	@GetMapping("/{boardId}") // 읽어오는거니까 getmapping
+	public ResponseEntity<?> getComments(@PathVariable("boardId") Long boardId) {
+		
+		//원게시글 존재 여부 확인
+		Optional<Board> _board = boardRepository.findById(boardId);
+			if(_board.isEmpty()) { // 참이면 해당 원 게시글이 존재하지 않음
+		return ResponseEntity.badRequest().body("해당 게시글이 존재하지 않습니다.");
+			}
+		
+		List<Comment> comments = commentRepository.findByBoard(_board.get());
+		
+		return ResponseEntity.ok(comments);
+		
 	}
 
 }
--- src/main/java/com/kmii/home/repository/CommentRepository.java
+++ src/main/java/com/kmii/home/repository/CommentRepository.java
@@ -2,10 +2,14 @@ package com.kmii.home.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.kmii.home.entity.Board;
 import com.kmii.home.entity.Comment;
+import java.util.List;
+
 
 public interface CommentRepository extends JpaRepository<Comment, Long>{
 	
-	
+	//댓글이 달린 원 게시글로 댓글 리스트 반환 메서드
+	List<Comment> findByBoard(Board board);
 
 }
